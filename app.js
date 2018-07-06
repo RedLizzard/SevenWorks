da89aa2f925cb3e7bee451afad6b7339ed4ae4d7
Jason <whispersinthesoundofsilence@gmail.com>
2018-07-06 19:16:54 +0100

    Exploring views, partials, and layouts for the first time; adding a navbar and a basic landing page; installing mongoose

--- app.js
+++ app.js
@@ -1,23 +1,30 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
 
 const app = express();
 
+//Handlebars middleware
+app.engine('handlebars', exphbs(    {defaultLayout: 'main'} ));
+app.set('view engine', 'handlebars');
+
 //How middleware works
-app.use((req, res, next)=>{
-    console.log(Date.now());
-    req.name = 'Harry Potter';
-    next();
-});
+// app.use((req, res, next)=>{
+//     console.log(Date.now());
+//     req.name = 'Harry Potter';
+//     next();
+// });
 
 // Index Route
 app.get('/', (req, res)=>{
-    console.log(req.name)
-    res.send('INDEX');
+    const title = 'Welcome';
+    res.render('index', {
+        title: title
+    });
 });
 
 //About Route
 app.get('/about', (req, res)=>{
-    res.send('ABOUT');
+    res.render('about');
 });
 
 const port = 5000;
