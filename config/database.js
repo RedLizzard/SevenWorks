e3768c327f5e35fc85f5ca2aff8449ea709d58d8
Jason <whispersinthesoundofsilence@gmail.com>
2018-07-09 11:36:56 +0100

    Fixing mLab database declaration

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 if(process.env.NODE_ENV === 'production'){
     module.exports = {mongoURI: 
-        'mongodb://<jason>:<Kendon2312>@ds131711.mlab.com:31711/redlizzard-diary'}
+        'mongodb://jason:Kendon2312@ds131711.mlab.com:31711/redlizzard-diary'}
 }
 else{
     module.exports = {mongoURI: 
