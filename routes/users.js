f36c81dc57d2b0bfe0160cb7ebcf1f4190bbfbe0
Jason <whispersinthesoundofsilence@gmail.com>
2018-07-09 15:47:50 +0100

    Validating an existing user on the home page

--- routes/users.js
+++ routes/users.js
@@ -91,7 +91,7 @@ router.post('/register', (req, res) => {
 router.get('/logout', (req, res)=>{
     req.logout();
     req.flash('success_msg', 'You are logged out');
-    res.redirect('/users/login');
+    res.redirect('/');
 });
 
 module.exports = router;
